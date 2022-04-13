e4a003d56d88ba648e132f27fc588825c4c1cba9
Andrew Zhulanov <aumakua.ru@gmail.com>
2022-04-13 14:41:47 +0500

    Tiny optimization

--- serpFilter.user.js
+++ serpFilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           serpFilter
 // @namespace      aumakua
-// @version        1.1.1
+// @version        1.1.2
 // @description    Remove crappy yandex zen (and any other sites) from yandex and google search results.
 // @description:ru Удаляет ссылки на Яндекс.Дзен (можно добавлять и другие сайты) из результатов поиска гугла и яндекса.
 // @author         aumakua
@@ -30,8 +30,8 @@
         const snippets = document.querySelectorAll(snippet_id);
         for (let snippet of snippets) {
             const links = snippet.querySelectorAll(link_id);
-            for (let piece of crap) {
-                for (let link of links) {
+            for (let link of links) {
+                for (let piece of crap) {
                     if (link.href.includes(piece)) {
                         snippet.style.display = 'none';
                         break;
