02667706a0a6c7e1a4f45cc60cf7d66a8dfdef8a
Andrey Zhulanov <aumakua.ru@gmail.com>
2025-04-29 13:39:30 +0500

    Revert update

--- serpFilter.user.js
+++ serpFilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           serpFilter
 // @namespace      aumakua
-// @version        1.2.1
+// @version        1.2.0
 // @description    Remove crappy yandex zen (and any other sites) from yandex and google search results.
 // @description:ru Удаляет ссылки на Яндекс.Дзен (можно добавлять и другие сайты) из результатов поиска гугла и яндекса.
 // @author         aumakua
@@ -22,7 +22,7 @@
         snippet_id = 'li:has(div.organic)';
     }
     else if (document.location.hostname.includes('google')) {
-        snippet_id = 'div#rso>div';
+        snippet_id = 'div.g';
     }
 
     const remove_crap = () => {
